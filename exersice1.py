9ab45fe1bd73484d0f5ac1501b1a087bd7d0986f
Ayeshan Kavindu <138565728+AyeshanKavindu@users.noreply.github.com>
2025-03-27 09:06:12 +0000

    hello2

---
+++ exersice1.py
@@ -0,0 +1,5 @@
+d_rate = 0.93
+amount_usd = int(input("Enter the amount:"))
+
+amount_eur = amount_usd * d_rate
+print(f"{amount_eur} EURO")
---
+++ hello2.py
@@ -0,0 +1,4 @@
+user_name = input("Enter Your Name")
+user_age = int(input("enter Your age"))
+
+print(f"Hello {user_name}. You are {user_age} years old")
