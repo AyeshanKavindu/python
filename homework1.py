b34a9337aa95e1042722ac4a89d10876c7064ad6
Ayeshan Kavindu <138565728+AyeshanKavindu@users.noreply.github.com>
2025-04-02 07:12:07 +0000

    h

---
+++ homework1.py
@@ -0,0 +1,9 @@
+capital_city = input("What is the capital city of Japan?  ")
+
+answer1 = "Tokyo"
+answer2 = "tokyo"
+
+if capital_city == answer1 or capital_city == answer2:
+    print("Correct")
+else:
+    print("Not Correct")
