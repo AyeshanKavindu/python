04aab3e2df738e8e5ed69a473b7fa2ca2338af44
Ayeshan Kavindu <138565728+AyeshanKavindu@users.noreply.github.com>
2025-04-15 19:00:43 +0000

    hh

--- homeworkgamesAge.py
+++ homeworkgamesAge.py
@@ -5,14 +5,13 @@ games = [
     {"name": "Free Fire", "minimum_age": 16}
 ]
 
-# Function to check if the user is old enough to play the game
+
 def check_game_age(game_age, user_age):
     return user_age >= game_age
 
-# Ask the user for their age
+
 user_age = int(input("Please enter your age: "))
 
-# Loop through each game and check if the user can play it
 for game in games:
     if check_game_age(game["minimum_age"], user_age):
         print(f"You can play {game['name']}.")
